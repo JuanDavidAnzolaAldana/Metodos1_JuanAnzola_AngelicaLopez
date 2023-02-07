aa5db9b3cba942b7620033225b58a978f495ee45
JuanDavidAnzolaAldana <j.anzolaa@uniandes.edu.co>
2023-02-06 19:26:31 -0500

    colisiones

--- taller1/colisiones.py
+++ taller1/colisiones.py
@@ -1,8 +1,10 @@
+#Problema 2.08 Choques de duración finita
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import tqdm
 import time
+k=1
 def dot(a:numpy.ndarray,b:numpy.ndarray)->float:
     if a.shape!=b.shape:
         raise Exception("Should have the same size")
@@ -17,6 +19,7 @@ class particle:
         self.vel=vel
         self.mass=mass
         self.r=radius
+        self.a=0
     def get_status(self)->numpy.ndarray:
         state=numpy.zeros((2,2))
         state[0]=self.pos
@@ -24,13 +27,15 @@ class particle:
         return state
     def collide(self,other)->None:
         if norm(self.pos-other.pos)<=self.r+other.r:
-            other.vel=(other.pos-self.pos)*3
+            other.a+=k*(norm(self.pos-other.pos)**2)*(other.pos-self.pos)/other.mass
     def interact(self,colliders:list)->None:
+        self.a=0
         for i in colliders:
             if i!=self:
                 i.collide(self)
     def move(self,dt:float)->None:
-        self.pos+=self.vel*dt
+        self.pos+=self.vel*dt+(self.a*(dt**2)/2)
+        self.vel+=self.a*dt
 class bounding_box:
     def __init__(self, size:tuple, pos:tuple):
         self.size=size
@@ -48,15 +53,6 @@ class bounding_box:
             other.vel[1]=-abs(other.vel[1])
         if other.pos[1]-other.r<=self.pos[1]:
             other.vel[1]=abs(other.vel[1])
-
-def testlib()->None:
-    print(type(numpy.zeros((2,2))))
-    for i in tqdm.tqdm(range(10)):
-        time.sleep(0.2)
-    fig=plt.figure(figsize=(5,5))
-    ax=fig.add_subplot()
-    ax.plot(numpy.arange(10),numpy.arange(10))
-    plt.show()
 def simulate(particles:list,bounds:bounding_box,maxtime:float,deltatime:float)->list:
     timeline=numpy.arange(0,maxtime,deltatime)
     colliders=particles.copy()
@@ -71,10 +67,10 @@ def simulate(particles:list,bounds:bounding_box,maxtime:float,deltatime:float)->
         for j in particles:
             j.move(deltatime)
     return (info,timeline)
-def simulation(tf:float,dt:float)->None:
+def simulation(tf:float,dt:float,fr:int)->None:
     box=bounding_box((40,40), (-20,-20))
     particles=[particle(numpy.array([-15.0,1.0]), numpy.array([10.0,0.0]), 1.0, 2.0),
-               particle(numpy.array([0.0,-1.5]), numpy.array([0.0,0.0]), 1.0, 2.0)]
+               particle(numpy.array([0.0,-2]), numpy.array([0.0,0.0]), 1.0, 2.0)]
     info,tl=simulate(particles,box,tf,dt)
     fig=plt.figure(figsize=(5,5))
     ax=fig.add_subplot()
@@ -83,13 +79,30 @@ def simulation(tf:float,dt:float)->None:
     def update(i):
         ax.clear()
         start()
-        ax.set_title(r'$ t=%.2f \ s$' %(tl[i]))
+        ax.set_title(r'$ t=%.2f \ s$' %(tl[i*fr]))
         for j in range(len(info)):
-            st=info[j][i]
+            st=info[j][i*fr]
             ax.add_patch(plt.Circle((st[0,0],st[0,1]),particles[j].r, fill=True, color='k'))
             ax.arrow(st[0,0],st[0,1],st[1,0],st[1,1],color='r',head_width=1,length_includes_head=True)
     start()
-    Animation = anim.FuncAnimation(fig,update,frames=len(tl),init_func=start,interval=1000*dt)
+    Animation = anim.FuncAnimation(fig,update,frames=int(len(tl)/fr),init_func=start,interval=1000*dt*fr)
     plt.show()
-#testlib()
-simulation(30,0.05)
+while True:
+    a=input("""\nProblema 2.08 Choques de duración finita\nSeleccione:
+1. Correr simulación.\n2. ¿Qué significa la constate K?\n3. Salir.\n""")
+    if a=="1":
+        try:
+            k=float(input("Ingrese la constante K.\n"))
+            simulation(float(input("Ingrese el tiempo a simular.\n")),
+float(input("Ingrese el tiempo de un paso de simulación.\n")),int(input("Ingrese la cantidad de pasos de simulación por fotograma.\n")))
+        except:
+            print("Tu entrada no es un número.")
+
+    elif a=="2":
+        print("Creo que el significado de la constante K es la resistencia a la deformación del material. Si un material tiene una K pequeña, este se deformará fácilmente y será más fácil atravesar el material en lugar de colisionar con el. Si K es un número grande, el material no se deformará y será más fácil chocar con él que atravesarlo.")
+    elif a=="3":
+        print("adios")
+        break
+    else:
+        print("opción inválida")
+
