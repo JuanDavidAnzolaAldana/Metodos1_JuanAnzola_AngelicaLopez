e935adcda2dc6803ea33298278fdf43b0c071600
JuanDavidAnzolaAldana <j.anzolaa@uniandes.edu.co>
2023-02-23 12:16:35 -0500

    derivada derecha actualizada

--- taller2/derivadaDerecha.py
+++ taller2/derivadaDerecha.py
@@ -12,14 +12,17 @@ def lagrange(pos,points):
     return suma
 func=None
 f=None
+interval=[0,1]
 def Interpolar():
     global func
     global f
+    global interval
     x=sympy.Symbol("x",real=True)
     ps=numpy.array([[1,0],[2,4],[3,1],[4,2]])
     f=sympy.simplify(lagrange(x,ps))
     func=sympy.lambdify([x],f,numpy)
-    px=numpy.linspace(0,5,50)
+    interval=[0,5]
+    px=numpy.linspace(0,5,100)
     py=func(px)
     plt.title(f)
     plt.plot(px,py)
@@ -30,22 +33,36 @@ def derivada(pos,f,h):
 def derivar():
     if func==None:
         return None
-    px=numpy.linspace(0,5,50)
+    px=numpy.linspace(interval[0],interval[1],100)
     py=func(px)
-    dy=derivada(px,func,0.001)
+    dy=derivada(px,func,0.01)
     plt.plot(px,py)
     plt.plot(px,dy)
     plt.show()
 def raiztangente():
     global f
     global func
+    global interval
     x=sympy.Symbol("x",real=True)
     f=sympy.sqrt(sympy.tan(x))
     func=sympy.lambdify([x],f,numpy)
-    px=numpy.linspace(0,5,50)
+    interval=[0.1,1.1]
+    px=numpy.linspace(0.1,1.1,100)
     py=func(px)
     plt.title(f)
     plt.plot(px,py)
     plt.show()
+def central(pos,f,h):
+    return (f(pos+h)-f(pos-h))/(2*h)
+def centrar():
+    if func==None:
+        return None
+    px=numpy.linspace(interval[0],interval[1],100)
+    py=func(px)
+    dy=central(px,func,0.01)
+    plt.plot(px,py)
+    plt.plot(px,dy)
+    plt.show()
 raiztangente()
 derivar()
+centrar()
